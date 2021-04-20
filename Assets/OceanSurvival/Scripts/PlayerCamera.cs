b9cd0cea6750b145c119f60f5a193bf48abb05f6
Cooper Saye <coopersaye@gmail.com>
2021-04-20 12:33:10 -0600

    Implement player camera.

---
+++ Assets/OceanSurvival/Scripts/PlayerCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace OceanSurvival
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Camera cam;
+        [SerializeField] private Transform playerTransform;
+
+        private Vector2 MinWorldPoint => cam.ScreenToWorldPoint(Vector2.zero);
+        private Vector2 MaxWorldPoint => cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        private void Update()
+        {
+            // Move to player position
+            Vector2 playerPosition = playerTransform.position;
+
+            // Calculate screen midpoint dimensions
+            Vector2 screenSize = MaxWorldPoint - MinWorldPoint;
+            float midWidth = screenSize.x / 2;
+            float midHeight = screenSize.y / 2;
+
+            // Get minimum and maximum camera positions
+            float minCameraX = Map.MinX + midWidth;
+            float maxCameraX = Map.MaxX - midWidth;
+            float minCameraY = Map.MinY + midHeight;
+            float maxCameraY = Map.MaxY - midHeight;
+
+            // Clamp camera position
+            float cameraX = minCameraX > maxCameraX ? 0 : Mathf.Clamp(playerPosition.x, minCameraX, maxCameraX);
+            float cameraY = minCameraY > maxCameraY ? 0 : Mathf.Clamp(playerPosition.y, minCameraY, maxCameraY);
+
+            // Move camera to position
+            transform.position = new Vector3(cameraX, cameraY, transform.position.z);
+        }
+    }
+}
