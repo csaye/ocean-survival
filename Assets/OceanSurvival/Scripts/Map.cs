70f3555b2d8296d16e587500d393758f2eb9a4a4
Cooper Saye <coopersaye@gmail.com>
2021-04-20 16:15:59 -0600

    Implement raft collisions.

--- Assets/OceanSurvival/Scripts/Map.cs
+++ Assets/OceanSurvival/Scripts/Map.cs
@@ -7,21 +7,37 @@ namespace OceanSurvival
     {
         [Header("References")]
         [SerializeField] private Tilemap groundTilemap;
-        [SerializeField] private Tile waterTile;
+        [SerializeField] private Tile waterTile, raftTile;
 
         public const int MinX = -16;
         public const int MinY = -16;
         public const int MaxX = 16;
         public const int MaxY = 16;
 
+        private const int RaftMinX = -1;
+        private const int RaftMinY = -1;
+        private const int RaftMaxX = 1;
+        private const int RaftMaxY = 1;
+
         private void Start()
         {
+            // Fill map with water
             for (int x = MinX; x < MaxX; x++)
             {
                 for (int y = MinY; y < MaxY; y++)
                 {
                     Vector3Int position = new Vector3Int(x, y, 0);
                     groundTilemap.SetTile(position, waterTile);
+                }
+            }
+
+            // Fill raft tiles
+            for (int x = RaftMinX; x < RaftMaxX; x++)
+            {
+                for (int y = RaftMinY; y < RaftMaxY; y++)
+                {
+                    Vector3Int position = new Vector3Int(x, y, 0);
+                    groundTilemap.SetTile(position, raftTile);
                 }
             }
         }
