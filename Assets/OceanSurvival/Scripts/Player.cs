24d874843aff31c5fca612dd9f95488b549cd481
Cooper Saye <coopersaye@gmail.com>
2021-04-20 12:32:26 -0600

    Implement player movement.

---
+++ Assets/OceanSurvival/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace OceanSurvival
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Rigidbody2D rb;
+
+        private const float Speed = 3;
+
+        private Vector2 direction;
+
+        private void Update()
+        {
+            // Get movement direction
+            direction = Vector2.zero;
+            if (Input.GetKey(KeyCode.W)) direction.y += 1;
+            if (Input.GetKey(KeyCode.S)) direction.y -= 1;
+            if (Input.GetKey(KeyCode.D)) direction.x += 1;
+            if (Input.GetKey(KeyCode.A)) direction.x -= 1;
+            direction.Normalize();
+
+            // Update rigidbody velocity
+            rb.velocity = direction * Speed;
+        }
+    }
+}
