62c4310dcab49e9e5ae6095c8765dbe7f4b0ff33
Cooper Saye <coopersaye@gmail.com>
2021-04-21 12:37:52 -0600

    Create main menu.

---
+++ Assets/OceanSurvival/Scripts/UI/MenuButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OceanSurvival.UI
+{
+    public class MenuButton : MonoBehaviour
+    {
+        public void LoadScene(string scene) => SceneManager.LoadScene(scene);
+        public void Quit() => Application.Quit();
+    }
+}
