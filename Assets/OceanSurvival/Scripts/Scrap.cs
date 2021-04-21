57bad30aa286b67f1465d24f96e99571f1cb9264
Cooper Saye <coopersaye@gmail.com>
2021-04-20 22:51:48 -0600

    Implement scrap.

---
+++ Assets/OceanSurvival/Scripts/Scrap.cs
@@ -0,0 +1,41 @@
+﻿using OceanSurvival.UI;
+using UnityEngine;
+
+namespace OceanSurvival
+{
+    public class Scrap : MonoBehaviour, IInteractable
+    {
+        [Header("Attributes")]
+        [SerializeField] private Item pickupItem;
+        
+        [Header("References")]
+        [SerializeField] private Rigidbody2D rb;
+
+        private const float Speed = 1;
+
+        public void Initialize(Vector2Int direction)
+        {
+            // Set rigidbody velocity
+            rb.velocity = (Vector2)direction * Speed;
+        }
+
+        public void Interact()
+        {
+            // Add item to inventory
+            Inventory.Instance.AddItem(pickupItem);
+
+            // Destroy self
+            Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            // Destroy self if out of bounds
+            Vector2 position = transform.position;
+            if (position.x < Map.MinX || position.x > Map.MaxX || position.y < Map.MinY || position.y > Map.MaxY)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
